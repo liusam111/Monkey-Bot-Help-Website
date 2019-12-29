acb0b8d1bfb6000563468e3c73bbb9bbd9a7e822
Xanaduanex <liusam111@gmail.com>
2019-12-28 17:38:37 -0800

    Added search bar

--- command_buttons.js
+++ command_buttons.js
@@ -1,20 +1,46 @@
 
-var acc = document.getElementsByClassName("command");
-var i;
-for(i = 0; i < acc.length; i++){
-    acc[i].onclick = function (){
-        var content = this.nextElementSibling;
-        this.classList.toggle("active");
-        content.classList.toggle("show");
+let commands = document.getElementsByClassName("command");
 
-        
-        if(content.style.maxHeight){
-            //Command is open, need to close
-            content.style.maxHeight = null;
-        } else {
-            //Command is closed, need to open
-            content.style.maxHeight = (content.scrollHeight + 50) + "px";
-        }
+for(let i = 0; i < commands.length; i++){
+    commands[i].addEventListener("click", toggleActive);
+}
+
+let searchbar = document.getElementsByClassName("search-bar");
+searchbar[0].addEventListener("keyup", search);
+
+
+function toggleActive(){
+    var content = this.nextElementSibling;
+    this.classList.toggle("active");
+    content.classList.toggle("show");
+
+    
+    if(content.style.maxHeight){
+        //Command is open, need to close
+        content.style.maxHeight = null;
+    } else {
+        //Command is closed, need to open
+        content.style.maxHeight = (content.scrollHeight + 50) + "px";
     }
+}
 
+function search(){
+    let commands = document.getElementsByClassName("command");
+    let searchbar = document.getElementsByClassName("search-bar");
+    
+    let input = searchbar[0].value.toLowerCase();
+
+    for(let i = 0; i < commands.length; i++){
+        let currCommand = commands[i];
+        let currText = currCommand.textContent.toLowerCase();
+
+        if(currText.indexOf(input) > -1){
+            currCommand.style.display = "";
+            currCommand.nextElementSibling.style.display = "";
+        } else {
+            currCommand.style.display = "none";
+            currCommand.nextElementSibling.style.display = "none";
+        }
+
+    }
 }
